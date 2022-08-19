d53255a629c47191dd254be55e6c75c6c9d83602
uma <varagamuma@gmail.com>
2022-08-19 20:06:34 +0530

    4.1 updated code

--- Week-4/4.1.js
+++ Week-4/4.1.js
@@ -1,6 +1,6 @@
 function getNumber(randomValue) {
     return new Promise((resolve, reject) => {
-        if (randomValue%5 == 0) {
+        if (randomValue % 5 == 0) {
             resolve(randomValue)
         } else {
             reject(randomValue)
@@ -8,8 +8,31 @@ function getNumber(randomValue) {
     })
 }
 var p = getNumber(5)
-p.then(x => console.log( "resolved"))
-    .catch(y => console.log( "rejected"))
+p.then(x => console.log("resolved"))
+    .catch(y => console.log("rejected"))
+//======================================USING CALL BACKS=========================================================
+function getNumber(value) {
+    if (value == 0) {
+        return null;
+    } else if (value % 5 == 0) {
+        return true
+    } else {
+        return false;
+    }
 
+}
+
+function displayResult(displayVal) {
+    if (displayVal == null) {
+        return "fullfilled";
+    } else if (displayVal) {
+        return "resolved";
+    } else {
+        return "rejected";
+    }
+}
+
+let result = getNumber(5);
+displayResult(result);
 
 
