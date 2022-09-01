6d616bec1138c853d826741c2787c4b5d56f5b1f
uma <varagamuma@gmail.com>
2022-09-01 08:16:35 +0530

    vowelcount using map api

---
+++ Week-5/5.2.js
@@ -0,0 +1,36 @@
+// Write a function called vowelCount which 
+// accepts a string and returns a map where the 
+// keys are numbers and the values are the count 
+// of the vowels in the string.Guidelines:
+
+// 1.JS function should have Map API implemented.
+// 2.Map’s set functionality 
+// should have been used.
+// 3.Bonus - if space and time complexity is taken care.
+// 4.Reference:
+
+function vowelCount(str) {
+    const vowelMap = new Map();
+    for (let char of str) {
+        let lowerCaseChar = char.toLowerCase()
+        if (isVowel(lowerCaseChar)) {
+            if (vowelMap.has(lowerCaseChar)) {
+                vowelMap.set(lowerCaseChar, 
+                vowelMap.get(lowerCaseChar) + 1);
+            } else {
+                vowelMap.set(lowerCaseChar, 1);
+            }
+        }
+    }
+    return vowelMap;
+}
+
+
+function isVowel(char) {
+    return "aeiou".includes(char);
+}
+
+var mapValue = vowelCount("Umaaaa");
+for(let [key,value] of mapValue){
+    console.log(key,value)
+}
