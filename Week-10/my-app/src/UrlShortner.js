e319242813d8e6a4c6256964f903e9afeafdecd9
uma <varagamuma@gmail.com>
2022-10-12 20:33:13 +0530

    shortner spinner changes

--- Week-10/my-app/src/UrlShortner.js
+++ Week-10/my-app/src/UrlShortner.js
@@ -1,15 +1,18 @@
 import { React, useState } from "react";
 import axios from 'axios';
 import validator from 'validator'
-
+import Spinner from "../src/Spinner";
+import CopyToClipboard from "react-copy-to-clipboard";
+import image from "../src/image.jpg";
 
 function UrlShortner() {
   const [userInput, setUserInput] = useState("");
   const [shortenedLink, setShortenedLink] = useState("")
   const [errorMessage, setErrorMessage] = useState('')
+  const [loading, setLoading] = useState('')
 
   const validate = (value) => {
-    
+
     if (validator.isURL(value)) {
       fetchData();
     } else {
@@ -20,18 +23,25 @@ function UrlShortner() {
 
   const fetchData = async () => {
     try {
+      setLoading(true)
       const response = await axios(
         `https://api.shrtco.de/v2/shorten?url=${userInput}`
       );
       setShortenedLink(response.data.result.full_short_link);
+      setLoading(false)
     } catch (e) {
       console.log(e);
     }
   };
 
   return (
-    <div className=" container h-screen flex justify-center items-center">
-      <div className=" text-center">
+    <div>
+      {loading && <Spinner text={"loading"} />}
+      {!loading && <div className=" text-center">
+        <div className="heading">
+          <h1 className="heading-text">More than just shorter Links</h1>
+          <img className="heading-image" src={image} />
+        </div>
         <h1 className=" text-2xl font-medium text-blue-500 mb-4">
           Our <span className=" text-yellow-400">URL Shortener</span>
         </h1>
@@ -51,13 +61,18 @@ function UrlShortner() {
             Submit URL
           </button>
           <div className="mt-5">
-          {errorMessage}
+            {errorMessage}
           </div>
           <div className="mt-5">
             {shortenedLink}
+            <CopyToClipboard text={shortenedLink}>
+              <button className="border-2 border-blue-500 text-blue-500 font-medium px-5 py-2 ml-4 rounded-md">
+                Copy URL to Clipboard
+              </button>
+            </CopyToClipboard>
           </div>
         </div>
-      </div>
+      </div>}
     </div>
   );
 }
